67e6f16bd6ff19f08bbcc24324f8d9be5d49d0a4
dev <dev123@gmail.com>
2025-08-03 16:38:57 +0530

    third

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ console.log("3rd commit");
 console.log("feature 1st commit");
 
 console.log("bug first commit");
-console.log("bug second commit")
+console.log("bug second commit");
+console.log("bug third commit")
